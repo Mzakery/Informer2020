5730c857d03871acd5db83855a2cc30dcd4a01f9
Mzakery <90835498+Mzakery@users.noreply.github.com>
2024-05-22 01:19:45 +0330

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -4,9 +4,9 @@ import pandas as pd
 import joblib
 import torch
 from torch.utils.data import Dataset, DataLoader
-# from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
-from utils.tools import StandardScaler, MinMaxScaler
+from utils.tools import StandardScaler
 from utils.timefeatures import time_features
 
 import warnings
@@ -200,15 +200,13 @@ class Dataset_Custom(Dataset):
                  freq='h', 
                  cols=None, 
                  kind_of_scaler = None,
-                 test_size = 0.2,
-                 take_data_instead_of_reading = False,
-                 direct_data = None):
+                 test_size = 0.2):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
-            self.seq_len = 24*4*4
-            self.label_len = 24*4
-            self.pred_len = 24*4
+            self.seq_len = 1*5*2
+            self.label_len = 1*1
+            self.pred_len = 1*1
         else:
             self.seq_len = size[0]
             self.label_len = size[1]
@@ -218,8 +216,6 @@ class Dataset_Custom(Dataset):
         type_map = {'train':0, 'val':1, 'test':2}
         self.set_type = type_map[flag]
 
-        self.take_data_instead_of_reading = take_data_instead_of_reading
-        self.direct_data = direct_data
         self.test_size = test_size
         self.train_size = 0.90 - self.test_size
         self.features = features
@@ -236,10 +232,7 @@ class Dataset_Custom(Dataset):
 
     def __read_data__(self):
         
-        if self.take_data_instead_of_reading:
-            df_raw = self.direct_data
-        else:
-            df_raw = pd.read_csv(os.path.join(self.root_path,
+        df_raw = pd.read_csv(os.path.join(self.root_path,
                                               self.data_path))
         '''
         df_raw.columns: ['date', ...(other features), target feature]
@@ -268,31 +261,37 @@ class Dataset_Custom(Dataset):
 
         
         if self.scale:
-            col_scaled = []
-            for col in df_data.columns:
-                col_data = df_data[[col]].values
-                if self.kind_of_scaler == 'MinMax':
-                    if col == self.target:
-                        self.scaler = MinMaxScaler()
+            if self.features=='S' or self.features=='MS':
+                col_scaled = []
+                for col in df_data.columns:
+                    col_data = df_data[[col]].values
+                    if self.kind_of_scaler == 'MinMax':
+                        if col == self.target:
+                            self.scaler = MinMaxScaler()
+                        else:
+                            scaler = MinMaxScaler()
                     else:
-                        scaler = MinMaxScaler()
-                else:
+                        if col == self.target:
+                            self.scaler = StandardScaler()
+                        else:
+                            scaler = StandardScaler()
                     if col == self.target:
-                        self.scaler = StandardScaler()
+                        self.scaler.fit(col_data[border1s[0]:border2s[0]])
+                        joblib.dump(self.scaler, os.path.join(self.root_path, 'scaler.pkl'))
+                        col_temp = self.scaler.transform(col_data)
                     else:
-                        scaler = StandardScaler()
-                if col == self.target:
-                    self.scaler.fit(col_data[border1s[0]:border2s[0]])
-                    joblib.dump(self.scaler, os.path.join(self.root_path, 'scaler.pkl'))
-                    col_temp = self.scaler.transform(col_data)
+                        scaler.fit(col_data[border1s[0]:border2s[0]])
+                        col_temp = scaler.transform(col_data)
+                    col_scaled.append(col_temp)
+                if len(col_scaled) == 1:
+                    data = col_scaled[0]
                 else:
-                    scaler.fit(col_data[border1s[0]:border2s[0]])
-                    col_temp = scaler.transform(col_data)
-                col_scaled.append(col_temp)
-            if len(col_scaled) == 1:
-                data = col_scaled[0]
+                    data = np.concatenate(col_scaled, axis = 1)
             else:
-                data = np.concatenate(col_scaled, axis = 1)
+                self.scaler = StandardScaler()
+                train_data = df_data[border1s[0]:border2s[0]]
+                self.scaler.fit(train_data.values)
+                data = self.scaler.transform(df_data.values)                
         else:
             data = df_data.values
             
